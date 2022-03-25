4023378aaada6e8dfa82fa6fa2606c3033d9acf9
Hadie Danker <hadie87@gmail.com>
2022-03-25 22:20:24 +0700

    Controller

--- src/RajaOngkirController.php
+++ src/RajaOngkirController.php
@@ -7,15 +7,28 @@ use Illuminate\Http\Request;
 
 class RajaOngkirController extends Controller{
 
+    protected $client;
     public function __construct() {
-        //
-    }
-
-    public function get_cost(Request $request){
         $key = env('RAJAONGKIR_API_KEY');
         $accountType = env('RAJAONGKIR_ACCOUNT_TYPE','pro');
-        $client = new RajaOngkir($key, $accountType);
+        $this->client = new RajaOngkir($key, $accountType);
+    }
 
+    public function get_cost( $request){
+
+        $metrics = isset($request['weight']) ? abs($request['weight']) : 0;
+        if (isset($request['metrics'])){
+            $metrics = $request['metrics'];
+        }
+        $origin = [
+            'subdistrict' => isset($request['origin']) ? abs($request['origin']) : null,
+        ];
+
+        $destination = [
+            'subdistrict' => isset($request['destination']) ? abs($request['destination'])  : null,
+        ];
+
+       return $this->client->getCost($origin,$destination,$metrics,$request['courier']);
 
     }
 }
